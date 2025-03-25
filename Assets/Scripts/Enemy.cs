2e03423ba5e86519dcdc2ca1b19ac3e27d81763c
JacksonBell09 <jacksonbellschool@gmail.com>
2025-03-25 12:36:52 -0400

    working on enemy scripting

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,9 +8,41 @@ public class Enemy : MonoBehaviour
     public GameObject[] wayPoints;
     private int currentWayPoint;
 
+    public Transform playerTransform;
+    public bool isChasing;
+    public float chaseDistance;
+
     // Update is called once per frame
     void Update()
     {
+        isChasing = Vector2.Distance(transform.position, playerTransform.position) < chaseDistance;
+        if (isChasing)
+        {
+            if (transform.position.x > playerTransform.position.x)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                transform.position += Vector3.left * EnemySpeed * Time.deltaTime;
+            }
+            if (transform.position.x < playerTransform.position.x)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                transform.position += Vector3.right * EnemySpeed * Time.deltaTime;
+            }
+            //for the Y axis
+            if (transform.position.y > playerTransform.position.y)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                transform.position += Vector3.up * EnemySpeed * Time.deltaTime;
+            }
+            if (transform.position.y < playerTransform.position.y)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                transform.position += Vector3.down * EnemySpeed * Time.deltaTime;
+            }
+        }
+        else
+        {
+
         if(Vector2.Distance(wayPoints[currentWayPoint].transform.position, transform.position) < .1f)   
         {
             currentWayPoint++;
@@ -20,7 +52,7 @@ public class Enemy : MonoBehaviour
                 currentWayPoint = 0;
             }
         }
-        
-        transform.position = Vector2.MoveTowards(transform.position, wayPoints[currentWayPoint].transform.position, Time.deltaTime * EnemySpeed);
+         transform.position = Vector2.MoveTowards(transform.position, wayPoints[currentWayPoint].transform.position, Time.deltaTime * EnemySpeed);
+        }
     }
 }
